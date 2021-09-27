73afe130b9a5c7bba2836e52943b76492ffb555b
venkat3877 <37626577+venkat3877@users.noreply.github.com>
2021-09-27 12:49:29 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,40 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+int a=10;
+short b=100;
+long c=9885471332L;
+byte d=4;
+float f=2.45f;
+double dd=2.4556662d;
+char cc='f';
+boolean istr=false;
+int n1=20,n2=50;
+if(n1>n2) {
+	System.out.println("n1is greater");
+}else {
+	System.out.println("n2 is greater");
+}
+
+String s1="ram king";
+float sss=a;
+System.out.println(1==1);
+if(1==1) 
+{
+	
+}
+
+int my=(int) dd;
+System.out.println(my);
+System.out.println(dd);
+if(a>b) {
+	System.out.println("hai");
+}else if(b>d) {
+	System.out.println("beye");
+}
+	}
+
+}
